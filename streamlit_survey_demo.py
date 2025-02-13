7a3b07c24d076ccb8d18241fcdbea9a3f71f4565
tanmayy24 <tk3309nyu.edu>
2025-02-12 20:11:23 -0800

    update in index

--- streamlit_survey_demo.py
+++ streamlit_survey_demo.py
@@ -16,7 +16,7 @@ model_folders = {
 }
 
 # Randomly select 10 prompts
-selected_prompts = prompts_df.sample(n=5, random_state=42).reset_index(drop=True)
+selected_prompts = prompts_df
 
 # Generate 30 (Prompt, Audio) pairs, 10 per model
 audio_samples = []
